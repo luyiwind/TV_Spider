dbbebeb35b83cffd948e0f8b584a2964422c341c
luyiwind <70989874+luyiwind@users.noreply.github.com>
2022-11-07 12:52:21 +0800

    Update douban.py

--- utils/douban.py
+++ utils/douban.py
@@ -25,7 +25,7 @@ def miniapp_request(path, query):
         headers = {
             "Host": "frodo.douban.com",
             "Connection": "Keep-Alive",
-            "Referer": "https://servicewechat.com/wx2f9b06c1de1ccfca/84/page-frame.html",
+            "Referer": "https://m.douban.com/subject_collection/movie_hot_gaia",
             "content-type": "application/json",
             "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat"
         }

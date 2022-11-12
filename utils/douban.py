3e9456576e186912b177afd2f36a4c9fdaf352a6
luyiwind <70989874+luyiwind@users.noreply.github.com>
2022-11-12 16:52:58 +0800

    Update douban.py

--- utils/douban.py
+++ utils/douban.py
@@ -142,11 +142,12 @@ def cate_filter(type, ext, pg, douban):
             if item.get("type", "") == "movie" or item.get("type", "") == "tv":
                 rating = item.get("rating", "").get("value", "") if item.get("rating", "") else ""
                 title = item.get("title", "")
+                info = item.get("episodes_info", "")
                 lists.append({
                     "vod_id": f'msearch:{item.get("type", "")}__{item.get("id", "")}',
                     "vod_name": title if title != "未知电影" else "暂不支持展示",
                     "vod_pic": item.get("pic", "").get("normal", ""),
-                    "vod_remarks": str(rating if rating else "暂无评分") + " " + " | ".join(honor["title"] for honor in item.get("honor_infos", []))
+                    "vod_remarks": str(rating if rating else "暂无评分") + " " + info + " " + " | ".join(honor["title"] for honor in item.get("honor_infos", []))
                 })
         result.setdefault("list", lists)
         return result
@@ -166,11 +167,12 @@ def subject_real_time_hotest():
         for item in res["subject_collection_items"]:
             if item.get("type", "") == "movie" or item.get("type", "") == "tv":
                 rating = item.get("rating", "").get("value", "") if item.get("rating", "") else ""
+                info = item.get("episodes_info", "")
                 lists.append({
                     "vod_id": f'msearch:{item.get("type", "")}__{item.get("id", "")}',
                     "vod_name": item.get("title", ""),
                     "vod_pic": item.get("pic", "").get("normal", ""),
-                    "vod_remarks": str(rating if rating else "暂无评分") + " " + " | ".join(honor["title"] for honor in item.get("honor_infos", []))
+                    "vod_remarks": str(rating if rating else "暂无评分") + " " + info + " " + " | ".join(honor["title"] for honor in item.get("honor_infos", []))
                 })
         return lists
     except Exception as e:

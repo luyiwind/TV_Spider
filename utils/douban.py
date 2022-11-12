3b8b81c628b42bb308a775f6ee57e58d122cc553
luyiwind <70989874+luyiwind@users.noreply.github.com>
2022-11-12 18:44:13 +0800

    Update douban.py

--- utils/douban.py
+++ utils/douban.py
@@ -146,7 +146,7 @@ def cate_filter(type, ext, pg, douban):
                 lists.append({
                     "vod_id": f'msearch:{item.get("type", "")}__{item.get("id", "")}',
                     "vod_name": title if title != "未知电影" else "暂不支持展示",
-                    "vod_lang": (info + " ") if info else "",
+                    "vod_lang": info if info else "",
                     "vod_pic": item.get("pic", "").get("normal", ""),
                     "vod_remarks": str(rating if rating else "暂无评分") + " " + " | ".join(honor["title"] for honor in item.get("honor_infos", []))
                 })
@@ -172,7 +172,7 @@ def subject_real_time_hotest():
                 lists.append({
                     "vod_id": f'msearch:{item.get("type", "")}__{item.get("id", "")}',
                     "vod_name": item.get("title", ""),
-                    "vod_lang": (info + " ") if info else "",
+                    "vod_lang": info if info else "",
                     "vod_pic": item.get("pic", "").get("normal", ""),
                     "vod_remarks": str(rating if rating else "暂无评分") + " " + " | ".join(honor["title"] for honor in item.get("honor_infos", []))
                 })
